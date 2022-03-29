09713e33aaafa8106e16bb54b339103735ffe5fa
Dean Tsonev <didotsonev@gmail.com>
2022-03-29 17:32:42 +0300

    uprajnenie 26.04

--- binarymode.php
+++ binarymode.php
@@ -1,7 +1,38 @@
 <?php
 include './dbconn.php';
-$quotequery=mysqli_query($conn, "SELECT *, authors.id AS aut_id FROM `authors` LEFT JOIN `quotes` ON authors.id=quotes.author_id ORDER BY RAND() LIMIT 1 ");
-$authorquery=mysqli_query($conn, "SELECT *, authors.id AS aut_id FROM `authors` LEFT JOIN `quotes` ON authors.id=quotes.author_id ORDER BY RAND() LIMIT 1 ");
+
+$quotesQuery = mysqli_query(
+    $conn,
+    "SELECT * FROM `quotes`"
+);
+$authorsQuery = mysqli_query(
+    $conn,
+    "SELECT * FROM `authors`"
+);
+
+// create new empty array
+$quotesArray = [];
+
+while ($quote = mysqli_fetch_assoc($quotesQuery)) {
+    // store the row as new key in the array
+    $quotesArray[] = $quote;
+}
+
+// create new empty array
+$authorsArray = [];
+
+while ($author = mysqli_fetch_assoc($authorsQuery)) {
+    // store the row as new key in the array
+    $authorsArray[] = $author;
+}
+
+echo '<pre>'. print_r($quotesArray[0], true) .'</pre>';
+
+$authorsCount = count($authorsArray);
+
+$randomAuthorKey = rand(0, $authorsCount - 1);
+
+echo '<pre>'. print_r($authorsArray[$randomAuthorKey], true) .'</pre>';
 ?>
 
 <html lang="en">
@@ -25,37 +56,48 @@ $authorquery=mysqli_query($conn, "SELECT *, authors.id AS aut_id FROM `authors` 
         <h3>Who said it?</h3>
         <div class="binaryquote" >
             <?php
-            while($row=mysqli_fetch_assoc($quotequery)){
-                echo $row['quote'];
-                $resultquote[]=$row['author_id'];
-                $resultquote[]=$row['name'];
-                $resultquote[]=$row['quote'];
-            }
+            echo $quotesArray[0]['quote'];
             ?>
         </div>
         <div class="binaryauthor">
             <?php
-            while($row=mysqli_fetch_assoc($authorquery)){
-                echo '<h3>'.$row['name'].'</h3>';
-                $resultauthor[]=$row['aut_id'];
-                $resultauthor[]=$row['name'];
-                $resultauthor[]=$row['quote'];
-            }
+                echo '<h3>'.$authorsArray[$randomAuthorKey]['name'].'</h3>';
             ?>
         </div>
+            <?php
+                $check = 0;
 
-            <button id="BtnYes" class="button yesbutton">Yes!</button>
-            <button id="BtnNo" class="button nobutton">No!</button>
+                // check if this is the author
+                if ($quotesArray[0]['author_id'] === $authorsArray[$randomAuthorKey]['id']) {
+                    $check = 1;
+                }
+
+                echo '<button id="BtnYes" class="button yesbutton" onclick="buttonClicked(1, '. $check .')" >Yes!</button>';
+                echo '<button id="BtnYes" class="button nobutton" onclick="buttonClicked(0, '. $check .')">No!</button>';
+            ?>
     </div>
-
-   <?php
-        echo '<pre>' . print_r($resultquote, true) . '</pre>';
-        echo '<pre>' . print_r($resultauthor, true) . '</pre>';
-    ?>
-
+    <div>
+<!--        <p class="answer" id="correct-answer">The answer is correct</p>-->
+<!--        <p class="answer" id="incorrect-answer">The answer is NOT correct</p>-->
+        <p class="answer" id="what-is-the-answer">The answer is NOT correct</p>
+    </div>
 </body>
+
+<script>
+    function buttonClicked(value, check) {
+        // show div
+
+        let answers = document.getElementsByClassName('answer');
+        for(i = 0; i < answers.length; i++) {
+            answers[i].style.display = 'none';
+        }
+
+        if (check === value) {
+            document.getElementById("correct-answer").style.display = "block";
+        } else {
+            document.getElementById("incorrect-answer").style.display = "block";
+        }
+    }
+</script>
+
 </html>
-
-
-
-
