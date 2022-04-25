8cd2fff00ae718fcd1a62a110a1b046448f3ee24
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-04-25 13:07:51 +0300

    reload page on start over btn. ISSET THERE IS A MUST...

--- binarymode.php
+++ binarymode.php
@@ -43,7 +43,7 @@ while($row=mysqli_fetch_assoc($authorsQuery)){
             if(!isset($_SESSION['key'])){
                 $key=0;
                 $_SESSION['key']=0;
-                $_SESSION['correct_Answers']=0;
+                $_SESSION['correct_Answers_Count']=0;
             } else{
                 $_SESSION['key']=$_SESSION['key'] + 1;
                 $key=$_SESSION['key'];
@@ -52,10 +52,19 @@ while($row=mysqli_fetch_assoc($authorsQuery)){
             //$_POST returns 1 or 0 as string that's why the sign is ==
             if($check==$_POST['answer']){
                 echo '<p class=\"display_answer_txt\">CORRECT</p>';
-                $_SESSION['correct_Answers'] = $_SESSION['correct_Answers'] + 1;
+                $_SESSION['correct_Answers_Count'] = $_SESSION['correct_Answers_Count'] + 1;
             } else{
                 echo '<p class=\"display_answer_txt\">INcorrect</p>';
             }
+
+            //isset is a MUST
+            if(isset($_POST['start_over_btn'])){
+                session_destroy();
+                header('Location: binarymode.php');
+            }
+
+
+
             if(key_exists($key,$quotesArray)){
                 $quote=$quotesArray[$key];
                 $currentQuoteAuthorId=$quote['author_id'];
@@ -75,11 +84,11 @@ while($row=mysqli_fetch_assoc($authorsQuery)){
                 echo "</form>";
                 }
             } else{
-                $correctAnswers=$_SESSION['correct_Answers'];
+                $correctAnswersCount=$_SESSION['correct_Answers_Count'];
                 echo "<form action='binarymode.php' method=\"post\">";
                     echo "<div class=\"notHidden\" id=\"endOfQuizResult\">";
                     echo "<label for=\"inputAnswersCount\">Correct Answers:</label>";
-                    echo "<input type=\"text\" id=\"inputAnswersCount\" name=\"inputAnswersCount\" value=\"$correctAnswers\" readonly>";
+                    echo "<input type=\"text\" id=\"inputAnswersCount\" name=\"inputAnswersCount\" value=\"$correctAnswersCount\" readonly>";
                     echo "<button name=\"start_over_btn\">Start over</button>";
                     echo "</div>";
                 echo "</form>";
