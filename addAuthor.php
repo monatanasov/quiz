721ad0abfd28d2ae60fc9189bbd7394597d81bbf
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-05-07 20:59:03 +0300

    show all authors using table inside authors page

--- addAuthor.php
+++ addAuthor.php
@@ -1,13 +1,7 @@
 <?php
-include './dbconn.php';
-mb_internal_encoding("UTF-8");
-
-
-
-
+    include './dbconn.php';
+    mb_internal_encoding("UTF-8");
 ?>
-
-
 
 <html lang="en">
 <head>
@@ -29,13 +23,12 @@ mb_internal_encoding("UTF-8");
             echo 'Author name must be between 1 and 255 characters long!';
         }
         if (mysqli_num_rows($authorCheckResult) >= 1){
-            echo 'This Author already exists!';
+            echo 'This Author already exists!'.'<br>';
         } else {
             $insertAuthorSql = "INSERT INTO `authors`(`id`, `name`) VALUES (NULL,'$authorName')";
             $insertAuthorQuery = mysqli_query($conn,$insertAuthorSql);
-            echo 'Author ' . $authorName . ' was successfully added';
+            echo 'Author ' . $authorName . ' was successfully added'.'<br>';
         }
-        echo '<pre>' . print_r($authorName, true) . '</pre>';
     }
 ?>
     <a href="./index.php">Main page</a><br>
@@ -48,5 +41,21 @@ mb_internal_encoding("UTF-8");
             <input type="submit" class="submitAuthor">
         </div>
     </form>
+    <table border = "1px solid black">
+        <tr>
+            <th>Author ID</th>
+            <th>Author Name</th>
+        </tr>
+        <?php
+        //get all Authors from DB
+        $showAllAuthorsTable = mysqli_query($conn,"SELECT * FROM `authors`");
+        //show all Authors using HTML Table for better view
+        while($row=mysqli_fetch_assoc($showAllAuthorsTable)){
+            echo '<tr><td>'.$row['id'].'</td>
+                     <td>'.$row['name'].'</td>
+                    </tr>';
+        }
+        ?>
+    </table>
 </body>
 </html>
