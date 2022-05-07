081e7487205717e3505bd846f58476d06db2ae50
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-05-07 20:40:19 +0300

    Checks if author exists and add to DB if not already there

--- addAuthor.php
+++ addAuthor.php
@@ -1,10 +1,8 @@
 <?php
 include './dbconn.php';
 mb_internal_encoding("UTF-8");
-//validation
-//normalization
-//author already exist? IFs
-//insert into DB
+
+
 
 ?>
 
@@ -19,23 +17,33 @@ mb_internal_encoding("UTF-8");
 <body>
 
 <?php
-
     if ($_POST) {
-        $trimmedAuthor = trim($_POST['authorName']);
-
-        echo '<pre>' . print_r($trimmedAuthor, true) . '</pre>';
-
+    $trimmedAuthorName = trim($_POST['authorName']);
+    $authorCheckQuery = "SELECT * FROM `authors` WHERE `name` = '$trimmedAuthorName'";
+    $authorCheckResult = mysqli_query($conn,$authorCheckQuery);
+        if (
+            !mb_strlen($trimmedAuthorName) >= 1
+            && !mb_strlen($trimmedAuthorName) <=255
+        ) {
+            echo 'Author name must be between 1 and 255 characters long!';
+        }
+        if (mysqli_num_rows($authorCheckResult) >= 1){
+            echo 'This Author already exists!';
+        } else {
+            $insertAuthorSql = "INSERT INTO `authors`(`id`, `name`) VALUES (NULL,'$trimmedAuthorName')";
+            $insertAuthorQuery = mysqli_query($conn,$insertAuthorSql);
+            echo 'Author ' . $trimmedAuthorName . ' was successfully added';
+        }
+        echo '<pre>' . print_r($trimmedAuthorName, true) . '</pre>';
     }
 ?>
-
-
     <a href="./index.php">Main page</a><br>
     <a href="./binarymode.php">Binarymode quiz</a>
     <form action="addAuthor.php" method="POST">
             <h2>Add new author</h2>
         <div id="addAuthorDiv">
             <b><label for="">Author name</label></b>
-            <input type="text" class="addAuthorName" name="authorName">
+            <input type="text" class="addAuthorName" name="authorName" value="">
             <input type="submit" class="submitAuthor">
         </div>
     </form>
