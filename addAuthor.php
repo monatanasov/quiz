d7ade18cf804e862fa10023e51d13491b1c119af
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-05-15 12:10:57 +0300

    correct PSR12 comments

--- addAuthor.php
+++ addAuthor.php
@@ -11,18 +11,18 @@
 </head>
 <body>
 <?php
-    //That variable will be used to set the Value of the text field where AuthorNames are filled.
-    //Using this outside of POST puts blank name inside our input TEXT field for AuthorName
+    // That variable will be used to set the Value of the text field where AuthorNames are filled.
+    // Using this outside of POST puts blank name inside our input TEXT field for AuthorName
     $wrongAuthor = '';
     if ($_POST) {
-    //remove white spaces before and after filled AuthorName
+    // remove white spaces before and after filled AuthorName
     $authorName = trim($_POST['authorName']);
-    //check if our Author exists in DB.
-    //If there's a match mysqli_num_rows becomes 1 because our Authors are Unique
-    //mysqli_num_rows returns 0 if there's no match.
+    // check if our Author exists in DB.
+    // If there's a match mysqli_num_rows becomes 1 because our Authors are Unique
+    // mysqli_num_rows returns 0 if there's no match.
     $authorCheckQuery = "SELECT * FROM `authors` WHERE `name` = '$authorName'";
     $authorCheckResult = mysqli_query($conn,$authorCheckQuery);
-    //this var will be used to check for Errors right before Inserting into DB
+    // this var will be used to check for Errors right before Inserting into DB
     $errors = [];
         if (
             // TODO: missing documentation for mb_strlen
@@ -31,18 +31,18 @@
         ) {
             $errors [] = 'Author name must be between 1 and 255 characters long!<br>';
         }
-        //return the text below as message to the user if he submits Author that already exists in DB
+        // return the text below as message to the user if he submits Author that already exists in DB
         if (mysqli_num_rows($authorCheckResult) >= 1) {
             $errors [] = 'This Author already exists!<br>';
         }
-        //if there are no errors - INSERT the new Author INTO DB
-        //mysqli_real_escape_string prevents SQL injection
+        // if there are no errors - INSERT the new Author INTO DB
+        // mysqli_real_escape_string prevents SQL injection
         if (empty($errors)) {
             $insertAuthorSql = 'INSERT INTO `authors`(`name`) VALUES ("'.
                 mysqli_real_escape_string($conn,$authorName).'")';
             $insertAuthorQuery = mysqli_query($conn,$insertAuthorSql);
             echo 'Author ' . "<b>" . $authorName . "</b>" . ' was successfully added!' . '<br>';
-            //check if there are errors on INSERT query
+            // check if there are errors on INSERT query
             if (mysqli_error($conn)) {
                 echo 'Error on adding Author Name in DB';
                 exit;
@@ -74,9 +74,9 @@
             <th>Author Name</th>
         </tr>
         <?php
-        //get all Authors from DB
+        // get all Authors from DB
         $showAllAuthorsTable = mysqli_query($conn,"SELECT * FROM `authors`");
-        //show all Authors using HTML Table for better view
+        // show all Authors using HTML Table for better view
         while ($row=mysqli_fetch_assoc($showAllAuthorsTable)) {
             echo '<tr>
                      <td>'.$row['id'].'</td>
--- addQuote.php
+++ addQuote.php
@@ -2,9 +2,9 @@
 include './dbconn.php';
 // TODO: missing documentation
 mb_internal_encoding("UTF-8");
-//get all rows from Authors Table
+// get all rows from Authors Table
 $dbAuthorNamesQuery = mysqli_query($conn,"SELECT * FROM `authors`");
-//put all records from Authors Table inside Array for later use
+// put all records from Authors Table inside Array for later use
 while ($row = mysqli_fetch_assoc($dbAuthorNamesQuery)) {
     $allAuthors[] = $row;
 }
@@ -20,7 +20,7 @@ while ($row = mysqli_fetch_assoc($dbAuthorNamesQuery)) {
           if ($_POST) {
             $quoteTxt = trim($_POST['quoteTxt']);
             $selectedAuthorId = (int) trim($_POST['selectedAuthorName']);
-            //TODO: change errors logic like Dodo suggested
+            // TODO: change errors logic like Dodo suggested
             $errors = [];
             if (
                 !mb_strlen ($quoteTxt) >= 1
@@ -48,7 +48,6 @@ while ($row = mysqli_fetch_assoc($dbAuthorNamesQuery)) {
                     }
                 }
             }
-              //echo '<pre>' . print_r($insertQuoteSql, true) . '</pre>';
         }
     echo '<pre>' . print_r($_POST, true) . '</pre>';
     ?>
@@ -62,7 +61,7 @@ while ($row = mysqli_fetch_assoc($dbAuthorNamesQuery)) {
             <textarea class="addQuoteTxt" id="addQuoteTxt" name="quoteTxt" cols="40" rows="5"></textarea><br>
             <b><label for="authorNameDropDown">Choose author name</label></b>
             <?php
-                //display all Author Names inside dropdown select tag
+                // display all Author Names inside dropdown select tag
                 echo '<select id="authorNameDropDown" name="selectedAuthorName">';
                 foreach ($allAuthors as $key => $author) {
                     echo '<option value="' . $author['id'] . '">' . $author['name'] . '</option>'.'<br>';
@@ -78,12 +77,12 @@ while ($row = mysqli_fetch_assoc($dbAuthorNamesQuery)) {
             <th>Quote</th>
         </tr>
         <?php
-            //get all Quotes from DB
+            // get all Quotes from DB
             $query = mysqli_query(
                 $conn,
                 "SELECT * FROM `quotes` LEFT JOIN `authors` ON quotes.author_id = authors.id ORDER BY name ASC"
             );
-            //show all Quotes using HTML Table for better view
+            // show all Quotes using HTML Table for better view
             while ($row = mysqli_fetch_assoc($query)){
                 echo '<tr><td>' . $row['name'] . '</td>
                      <td>' . $row['quote'] . '</td> 
