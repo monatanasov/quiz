94306eac8374c374f23701ca29f7e8273fbfb68a
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-05-16 20:33:23 +0300

    instructions and minor changes

--- authors/index.php
+++ authors/index.php
@@ -20,22 +20,24 @@
     </head>
     <body>
         <a href="./createAuthor.php">Create Author</a><br>
-
         <table border = "1px solid black">
             <tr>
                 <th>Author ID</th>
                 <th>Author Name</th>
             </tr>
-        <?php
-            $showAllAuthorsTable = mysqli_query($conn, "SELECT * FROM `authors`");
-            while ($row = mysqli_fetch_assoc($showAllAuthorsTable)) {
-                echo '<tr>
-                         <td><a href="./deleteAuthor.php?id='.$row['id'].'">'.$row['id'].'</a></td>
-                         <td><a href="./editAuthor.php?id='.$row['id'].'">'.$row['name'].'</a></td>
-                      </tr>';
-            }
-        ?>
+            <?php
+                $showAllAuthorsTable = mysqli_query($conn, "SELECT * FROM `authors`");
+                while ($row = mysqli_fetch_assoc($showAllAuthorsTable)) {
+                    echo '<tr>
+                             <td><a href="./deleteAuthor.php?id='.$row['id'].'">'.$row['id'].'</a></td>
+                             <td><a href="./editAuthor.php?id='.$row['id'].'">'.$row['name'].'</a></td>
+                          </tr>';
+                }
+            ?>
         </table>
+
+        <h3>To delete author - click on any "id" in "Author ID" column</h3>
+        <h3>To edit author - click on any "name" in "Author Name" column</h3>
 
     </body>
 </html>
