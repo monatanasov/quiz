351f05f3bb46e3e41569bef2fd1093701939106a
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-05-21 14:20:11 +0300

    delete now works in index page too

--- authors/edit.php
+++ authors/edit.php
@@ -1,78 +1,36 @@
 <?php
-    // TODO: where should I put session_destroy?
-    session_start();
-    include '../dbconn.php';
-    mb_internal_encoding("UTF-8");
-
 echo '<pre>' . print_r($_POST, true) . '</pre>';
-    
-?>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8" >
-        <link rel="stylesheet" href="./css/myquiz.css">
-        <title>Edit Author</title>
-    </head>
-    <body>
-        <a href="./index.php">Authors</a><br>
+    if ($_POST) {
+        $errors = [];
+        $postAuthorName = trim($_POST['updateAuthor']);
+        $authorNameLength = mb_strlen($postAuthorName);
+        $authorId = $_SESSION['editableAuthorId'];     // TODO: PASS ID
 
-        <?php
-            // set blank name on input text field when submitting the form / $_POST
-            $editableAuthorName = '';
+        $authorCheckQuery = "SELECT * FROM `authors` WHERE `name` = '$postAuthorName'";
+        $authorCheckResult = mysqli_query($conn,$authorCheckQuery);
 
-            if ($_GET) {
-                $intGetAuthorId = (int) $_GET['id'];
-                $authorNameSql = "SELECT * FROM `authors` WHERE `id` = $intGetAuthorId";
-                $authorNameQuery = mysqli_query($conn, $authorNameSql);
+        if (!$authorNameLength >= 1 && !$authorNameLength <=255) {
+            $errors[] =  'Author name must be between 1 and 255 characters long!' . '<br>';
+        }
 
-                while ($row = mysqli_fetch_assoc($authorNameQuery)) {
-                    $editableAuthorName = $row['name'];
-                    $_SESSION['editableAuthorId'] = (int) $row['id'];
-                }
+        if (mysqli_num_rows($authorCheckResult) >= 1) {
+            $errors[] = 'This Author already exists!' . '<br>';
+        }
 
-            } else if ($_POST) {
-                $errors = [];
-                $postAuthorName = trim($_POST['authorName']);
-                $authorNameLength = mb_strlen($postAuthorName);
-                $authorId = $_SESSION['editableAuthorId'];
+        if (empty($errors)) {
+            $updateAuthorSql = "UPDATE `authors` SET `name`='$postAuthorName' WHERE `id` = $authorId";
+            mysqli_query($conn,$updateAuthorSql);
+            echo 'Your Author was successfully edited!';
 
-                $authorCheckQuery = "SELECT * FROM `authors` WHERE `name` = '$postAuthorName'";
-                $authorCheckResult = mysqli_query($conn,$authorCheckQuery);
-
-                if (!$authorNameLength >= 1 && !$authorNameLength <=255) {
-                    $errors[] =  'Author name must be between 1 and 255 characters long!' . '<br>';
-                }
-
-                if (mysqli_num_rows($authorCheckResult) >= 1) {
-                    $errors[] = 'This Author already exists!' . '<br>';
-                }
-
-                if (empty($errors)) {
-                    $updateAuthorSql = "UPDATE `authors` SET `name`='$postAuthorName' WHERE `id` = $authorId";
-                    mysqli_query($conn,$updateAuthorSql);
-                    echo 'Your Author was successfully edited!';
-
-                    if (mysqli_error($conn)) {
-                        echo 'No database connection in editAuthor page';
-                        exit;
-                    }
-                    session_destroy(); // TODO: sess_dest here?
-                } else {
-                    $editableAuthorName = $postAuthorName;
-                    foreach ($errors as $error) {
-                        echo $error;
-                    }
-                }
+            if (mysqli_error($conn)) {
+                echo 'No database connection in editAuthor page';
+                exit;
             }
-        ?>
-        <form action="edit.php" method="POST">
-            <h2>Edit Author</h2>
-            <div id="editAuthorDiv">
-                <b><label for="editAuthorName">Author name</label></b>
-                <?php echo '<input type="text" class="editAuthorName" id="editAuthorName" name="authorName" value="'.
-                    $editableAuthorName .'">'?>
-                <input type="submit" class="editAuthor" value="Edit">
-            </div>
-        </form>
-    </body>
-</html>
+        } else {
+            $editableAuthorName = $postAuthorName;
+            foreach ($errors as $error) {
+                echo $error;
+            }
+        }
+    }
+?>
--- authors/index.php
+++ authors/index.php
@@ -14,7 +14,7 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8" >
-        <link rel="stylesheet" href="./css/myquiz.css">
+        <link rel="stylesheet" href="../css/myquiz.css">
         <title>CRUD Authors</title>
     </head>
     <body>
@@ -28,13 +28,15 @@
                 echo $_SESSION['message'];
                 $showAllAuthorsTable = mysqli_query($conn, "SELECT * FROM `authors`");
                 while ($row = mysqli_fetch_assoc($showAllAuthorsTable)) {
+                echo '<form action="delete.php" method="POST">';
                     echo '<tr>
                              <td><a href="./show.php?id='.$row['id'].'">'.$row['id'].'</a></td>
                              <td><a href="./show.php?id='.$row['id'].'">'.$row['name'].'</a></td>';
+                    echo '<td><input type="text" name="authorId" value="'.$row['id'].'" readonly hidden></td>';
                     echo '<td><input type="submit" class="deleteAuthor" value="delete"></td>';
                     echo '</tr>';
+                echo '</form>';
                 }
-            echo '<pre>' . print_r($_SESSION['message'], true) . '</pre>';
             ?>
         </table>
     </body>
