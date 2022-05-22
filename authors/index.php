628c3ceaeb0f5da259869878623e5fa2d4dab96f
Simeon Atanasov <simeon.g.atanasov@gmail.com>
2022-05-22 14:25:08 +0300

    hide borders of hidden form elements and get all buttons inside table columns. TODO: CLEAR SPACING

--- authors/index.php
+++ authors/index.php
@@ -19,10 +19,12 @@
     </head>
     <body>
         <a href="create.php">Create Author</a><br>
-        <table border = "1px solid black">
+        <table>
             <tr>
                 <th>Author ID</th>
                 <th>Author Name</th>
+                <th>Delete Author</th>
+                <th>Edit Author</th>
             </tr>
             <?php
                 if (isset($_SESSION['message'])) {
@@ -36,17 +38,24 @@
                 if (mysqli_num_rows($allAuthorsTable) > 0) {
                     while ($row = mysqli_fetch_assoc($allAuthorsTable)) {
                         echo '<tr>';
-                        echo '<td><a href="./show.php?id='.$row['id'].'">'.$row['id'].'</a></td>';
-                        echo '<td><a href="./show.php?id='.$row['id'].'">'.$row['name'].'</a></td>';
-                        echo '<form action="delete.php" method="POST">';
-                            echo '<td><input type="text" name="authorId" value="'.$row['id'].'" readonly hidden></td>';
-                            echo '<td><input type="submit" class="deleteAuthor" value="delete"></td>';
-                        echo '</form>';
-                        echo '<form action="show.php" method="POST">';
-                            echo '<td><input type="text" name="authorId" value="'.$row['id'].'" readonly hidden></td>';
-                            echo '<td><input type="text" name="authorName" value="'.$row['name'].'" readonly hidden></td>';
-                            echo '<td><input type="submit" class="deleteAuthor" value="edit"></td>';
-                        echo '</form>';
+                            echo '<td><a href="./show.php?id='.$row['id'].'">'.$row['id'].'</a></td>';
+                            echo '<td><a href="./show.php?id='.$row['id'].'">'.$row['name'].'</a></td>';
+
+                            echo '<td>';
+                            echo '<form action="delete.php" method="POST">';
+                                echo '<input type="text" name="authorId" value="'.$row['id'].'" readonly hidden>';
+                                echo '<input type="submit" class="deleteAuthor" value="delete">';
+                            echo '</form>';
+                            echo '</td>';
+
+                            echo '<td>';
+                            echo '<form action="show.php" method="POST">';
+                                echo '<input type="text" name="authorId" value="'.$row['id'].'" readonly hidden>';
+                                echo '<input type="text" name="authorName" value="'.$row['name'].'" readonly hidden>';
+                                echo '<input type="submit" class="deleteAuthor" value="edit">';
+                            echo '</form>';
+                            echo '</td>';
+
                         echo '</tr>';
                     }
                 }
